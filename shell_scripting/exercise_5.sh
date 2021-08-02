3fa688c1be35161c282eaa7d213bfdcb27c93379
techtechtechtech <87956751+techtechtechtech@users.noreply.github.com>
2021-08-02 02:59:51 -0500

    Create exercise_5.sh

---
+++ shell_scripting/exercise_5.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
+# write your code here
+ISAY=
+ISAY=
+
+# Test code - do not modify
+echo "Warren Buffett said:"
+echo $BUFFETT
+echo "and I say:"
+echo $ISAY
