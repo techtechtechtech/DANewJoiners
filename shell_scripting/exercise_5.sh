c8b711bb25e3ca9e2186ce470f0bb5b54c4da2bb
techtechtechtech <87956751+techtechtechtech@users.noreply.github.com>
2021-08-02 03:06:06 -0500

    Update exercise_5.sh

--- shell_scripting/exercise_5.sh
+++ shell_scripting/exercise_5.sh
@@ -2,8 +2,10 @@
 
 BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
 # write your code here
-ISAY=
-ISAY=
+ISAY=$BUFFETT
+change1=${ISAY[@]//}
+change2=${change1[@]///}
+ISAY=${change2[@]//}
 
 # Test code - do not modify
 echo "Warren Buffett said:"
